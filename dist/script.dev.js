a35e1511e28f940d3fe5cd0f172228597f639d81
nuke7 <gombos.marton@icloud.com>
2021-01-14 12:30:49 +0100

    solved with background images

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,27 +1,28 @@
+"use strict";
+
 var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-
   // If we need pagination
+
   /* spaceBetween: 30, */
   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    renderBullet: function (index, className) {
+    renderBullet: function renderBullet(index, className) {
       return '<span class="' + className + '">' + (index + 1) + "</span>";
-    },
+    }
   },
-
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
+    prevEl: ".swiper-button-prev"
+  }
   /*   // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
   }, */
+
 });
